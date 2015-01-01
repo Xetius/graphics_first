e6091ffc3b1735b64ca0a64e36efaf6348cebcbf
Chris Hudson <chris@xetius.com>
2015-01-01 21:09:02 +0000

    initial build

---
+++ src/main/java/com/xetius/hello3d/Hello3d.java
@@ -0,0 +1,13 @@
+package com.xetius.hello3d;
+
+
+public class Hello3d {
+    public Hello3d() {
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        new Hello3d();
+    }
+
+}
