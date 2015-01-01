3baa5bac440e09b657d43d29cb2b065e19f0c817
Chris Hudson <chris@xetius.com>
2015-01-01 22:00:26 +0000

    Window drawing basics

--- src/main/java/com/xetius/hello3d/Hello3d.java
+++ src/main/java/com/xetius/hello3d/Hello3d.java
@@ -1,9 +1,30 @@
 package com.xetius.hello3d;
 
+import com.jogamp.newt.event.WindowAdapter;
+import com.jogamp.newt.event.WindowEvent;
+import com.jogamp.newt.opengl.GLWindow;
+
+import javax.media.opengl.GLCapabilities;
+import javax.media.opengl.GLProfile;
+import javax.media.opengl.awt.GLCanvas;
 
 public class Hello3d {
     public Hello3d() {
-        System.out.println("Hello World");
+        GLProfile profile = GLProfile.getDefault();
+        GLCapabilities capabilities = new GLCapabilities(profile);
+
+        GLWindow window = GLWindow.create(capabilities);
+
+        GLCanvas canvas = new GLCanvas(capabilities);
+        window.setSize(300, 300);
+        window.setVisible(true);
+        window.setTitle("NEWT Window Test");
+
+        window.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
     }
 
     public static void main(String[] args) {
